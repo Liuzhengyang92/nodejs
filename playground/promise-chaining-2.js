6b41efe537a7e0b45e7172dc3f48f2ae273deddf
felixatchur <felix@churinc.com>
2020-03-16 22:11:46 +1100

    migrate promise code

---
+++ playground/promise-chaining-2.js
@@ -0,0 +1,24 @@
+require('../task-manager/src/db/mongoose')
+const Task = require('../task-manager/src/models/task')
+
+// Task.findByIdAndDelete('5e62ddabc905317f28e217c6', { completed: false }).then((task) => {
+//   console.log(task)
+//   return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//   console.log(result)
+// }).catch((e) => {
+//   console.log(e)
+// })
+
+
+const DeleteTaskAndCount = async (id, completed) => {
+  const tasks = await Task.findByIdAndDelete(id)
+  const count = await Task.countDocuments({completed})
+  return count
+}
+
+DeleteTaskAndCount('5e62dde50a38557f3cba50a7', false).then((count) => {
+  console.log(count)
+}).catch(e => {
+  console.log(e)
+})
